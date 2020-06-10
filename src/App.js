f916e8c03eebda24694865fe0a64fe44dfd18221
Jasper Caballero <jvicaballero@gmail.com>
2020-06-09 20:00:51 -0400

    I can add my user stories

--- src/App.js
+++ src/App.js
@@ -2,6 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/*
+
+We will be making an application that displays a number value to the screen
+
+This is what we will be able to do as a user
+
+As a user...
+
+1) I can see the most up-to-date number value on the screen
+
+2)  I can click a button to increment num by 1
+
+3) I can click a button to decrement num by 1
+
+4) I can enter any number to increase the number value by that amount
+
+5) I can enter any number (amount) to decrease the number value by that amount
+*/
+
 function App() {
   return (
     <div className="App">
